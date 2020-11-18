46149abd4ee61d4d6d880874ffd0ebd61a6b0230
vitasso13 <43220494+vitasso13@users.noreply.github.com>
2020-11-17 22:38:17 -0300

    signup fix

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -19,11 +19,14 @@ class Signup extends Component {
 
     handleChange = e => this.setState({ [e.target.name]: e.target.value, error: "" });
 
-    signup = async(e) => {
+    signup = async (e) => {
         e.preventDefault();
         const { username, password, phone, name } = this.state;
         if (!username || !password || !phone || !name) {
-            return this.setState({ error: "Complete todos os campos!" });
+            this.setState(        
+                { flash: { status: 'is-danger', msg: 'Por favor, complete todos os campos' } }
+              );
+            
         } else {
             console.log("chegou aqui")
 
@@ -36,80 +39,87 @@ class Signup extends Component {
             }
             await axios.post('http://localhost:3001/users', thisNewUser)
 
-            
+            this.setState(
+                { flash: { status: 'is-success', msg: 'Cadastro realizado com sucesso' } }
+              );
 
         }
 
     }
 
-render() {
-    return !this.props.context.user ? (
-        <>
-            <div className="hero is-primary ">
-                <div className="hero-body container">
-                    <h4 className="title">Cadastre-se</h4>
-                </div>
-            </div>
-            <br />
-            <br />
-            <form onSubmit={this.signup}>
-                <div className="columns is-mobile is-centered">
-                    <div className="column is-one-third">
-                        <div className="field">
-
-                            <label className="label">Email: </label>
-                            <input
-                                className="input"
-                                type="email"
-                                name="username"
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                        <div className="field">
-                            <label className="label">Senha: </label>
-                            <input
-                                className="input"
-                                type="password"
-                                name="password"
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                        <div className="field">
-                            <label className="label">Telefone: </label>
-                            <input
-                                className="input"
-                                type="text"
-                                name="phone"
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                        <div className="field">
-                            <label className="label">Nome: </label>
-                            <input
-                                className="input"
-                                type="text"
-                                name="name"
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                        {this.state.error && (
-                            <div className="has-text-danger">{this.state.error}</div>
-                        )}
-                        <div className="field is-clearfix">
-                            <button
-                                className="button is-primary is-outlined is-pulled-right"
-                            >
-                                Entrar
-                </button>
-                        </div>
+    render() {
+        return !this.props.context.user ? (
+            <>
+                <div className="hero is-primary ">
+                    <div className="hero-body container">
+                        <h4 className="title">Cadastre-se</h4>
                     </div>
                 </div>
-            </form>
-        </>
-    ) : (
-            <Redirect to="/login" />
-        );
-}
+                <br />
+                <br />
+                <form onSubmit={this.signup}>
+                    <div className="columns is-mobile is-centered">
+                        <div className="column is-one-third">
+                            <div className="field">
+
+                                <label className="label">Email: </label>
+                                <input
+                                    className="input"
+                                    type="email"
+                                    name="username"
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="field">
+                                <label className="label">Senha: </label>
+                                <input
+                                    className="input"
+                                    type="password"
+                                    name="password"
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="field">
+                                <label className="label">Telefone: </label>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    name="phone"
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="field">
+                                <label className="label">Nome: </label>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    name="name"
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            {this.state.error && (
+                                <div className="has-text-danger">{this.state.error}</div>
+                            )}
+                            {this.state.flash && (
+                                <div className={`notification ${this.state.flash.status}`}>
+                                    {this.state.flash.msg}
+                                </div>
+                            )}
+                            <div className="field is-clearfix">
+                                <button
+                                    className="button is-primary is-outlined is-pulled-right"
+                                >
+                                    Cadastrar
+                </button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </>
+        ) : (
+                <Redirect to="/login" />
+            );
+    }
 }
 
 export default withContext(Signup);
